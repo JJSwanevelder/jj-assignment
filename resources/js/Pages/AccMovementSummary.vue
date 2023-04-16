e48f929b1ef40e5ab1831a130c6500de270a6507
Jan Swanevelder <jswanevelder@2u.com>
2023-04-16 17:43:06 +0200

    FrontEnd Views - Dashboard servers as account listing page - Account Movement Summary page

---
+++ resources/js/Pages/AccMovementSummary.vue
@@ -0,0 +1,28 @@
+<script>
+import {defineComponent} from "vue";
+import AppLayout from '@/Layouts/AppLayout.vue';
+import AccMovementSummaryTable from "@/Components/AccMovementSummaryTable.vue";
+
+export default defineComponent({
+    components: {
+        AccMovementSummaryTable,
+        AppLayout
+    },
+    props: {
+        user_account: Object,
+        daily_summary: Array,
+    },
+});
+</script>
+
+<template>
+    <AppLayout title="Movement Summary">
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <AccMovementSummaryTable :sub-userAccount="user_account" :sub-dailySummary="daily_summary"/>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,22 +1,34 @@
 <script setup>
+import {defineComponent} from "vue";
 import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
+
+defineProps({
+    userAccounts: Array,
+});
 </script>
 
 <template>
     <AppLayout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
-
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-7xl mx-auto">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+                    <UserAccountsTable :sub-userAccounts="userAccounts"/>
                 </div>
             </div>
         </div>
     </AppLayout>
 </template>
+
+<script>
+import { defineComponent } from 'vue'
+import UserAccountsTable from "@/Components/UserAccountsTable.vue";
+
+export default defineComponent({
+    components: {
+        UserAccountsTable,
+    },
+    props: {
+        userAccount: Array,
+    },
+})
+</script>
